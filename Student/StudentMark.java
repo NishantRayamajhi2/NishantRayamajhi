f20e23492c9e676c3e42c079caf5ab595298dd6f
NishantRayamajhi2 <nishantrayamajhi202@gmail.com>
2024-11-03 18:13:14 +0545

    some error solved
    some error solved

--- Student/StudentMark.java
+++ Student/StudentMark.java
@@ -12,16 +12,19 @@ public class StudentMark {
         float[] marks = new float[numberOfStudents];
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give marks for 30 students");
-        float mean =0;
         for (int i = 0; i < numberOfStudents; i++) {
             System.out.print("Enter mark for student " + (i + 1) + ": ");
             marks[i] = scanner.nextFloat();
         }
-        
+        float total=0;
+        for (int i = 0; i < numberOfStudents; i++) {
+        total=total + marks[i];
+        }  
         System.out.println("Entered marks:");
         for (float mark : marks) {
             System.out.println(mark);
         }
+        float mean =0;
         mean=total/numberOfStudents;
             System.out.printIn(mean);
             
